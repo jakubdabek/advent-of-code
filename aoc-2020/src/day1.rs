293fdae239431c8ceea2d21afb0f2b567d747b64
Jakub Dąbek <jakub.dabek@gmail.com>
2020-12-01 19:30:10 +0100

    Add 2020/day1

---
+++ aoc-2020/src/day1.rs
@@ -0,0 +1,60 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use std::collections::HashSet;
+
+#[aoc_generator(day1)]
+pub fn generate(s: &str) -> Vec<i32> {
+    s.lines()
+        .map(str::parse)
+        .collect::<Result<Vec<_>, _>>()
+        .expect("couldn't parse number")
+}
+
+#[aoc(day1, part1, online)]
+pub fn day1_part1_online(values: &[i32]) -> i32 {
+    let mut seen = HashSet::new();
+
+    for v in values.iter() {
+        let inverted = 2020 - v;
+        if seen.contains(&inverted) {
+            return v * inverted;
+        } else {
+            seen.insert(v);
+        }
+    }
+
+    panic!("couldn't find value")
+}
+
+#[aoc(day1, part1, premade)]
+pub fn day1_part1_premade(values: &[i32]) -> i32 {
+    let seen: HashSet<_> = values.iter().copied().collect();
+
+    for v in values.iter() {
+        let inverted = 2020 - v;
+        if seen.contains(&inverted) {
+            return v * inverted;
+        }
+    }
+
+    panic!("couldn't find value")
+}
+
+#[aoc(day1, part2)]
+pub fn day1_part2(values: &[i32]) -> i32 {
+    let seen: HashSet<_> = values.iter().copied().collect();
+
+    for v1 in values.iter() {
+        for v2 in values.iter() {
+            if v1 == v2 {
+                continue;
+            }
+
+            let inverted = 2020 - v1 - v2;
+            if seen.contains(&inverted) {
+                return v1 * v2 * inverted;
+            }
+        }
+    }
+
+    panic!("couldn't find the value")
+}
