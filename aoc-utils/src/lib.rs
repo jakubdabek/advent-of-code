f93ea420619aae016115d01bef98d7a27f2e76e1
Jakub Dąbek <jakub.dabek@gmail.com>
2020-12-02 14:27:51 +0100

    Add 2020/day2/part1 with lifetime problems

--- aoc-utils/src/lib.rs
+++ aoc-utils/src/lib.rs
@@ -1,5 +1,10 @@
+use std::convert::TryFrom;
 use std::str::FromStr;
 
 pub fn parse_lines<T: FromStr<Err = E>, E>(s: impl AsRef<str>) -> Result<Vec<T>, E> {
     s.as_ref().lines().map(str::parse).collect()
+}
+
+pub fn try_from_lines<'a, T: TryFrom<&'a str, Error = E>, E>(s: &'a str) -> Result<Vec<T>, E> {
+    s.lines().map(T::try_from).collect()
 }
